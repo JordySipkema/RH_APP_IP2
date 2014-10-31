2b4aaae434fb267eef7934d0e3e07ea583b55028
JordySipkema <jordysipkema@gmail.com>
2014-10-31 14:39:37 +0100

    dbfix 2

--- Mallaca/DBConnect.cs
+++ Mallaca/DBConnect.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Mallaca.Usertypes;
-using MySql.Data.MySqlClient;
-using System.Globalization;
-using Newtonsoft.Json.Linq;
+
 namespace Mallaca
 {
     public struct SessionData
